d0a0f0ad73ca5d08f64db106cea2111dec45a4c7
Fish (David B. Trout) <fish@infidels.org>
2016-12-26 14:51:45 -0800

    Remove file that shouldn't have been added

--- AutoBuildCount.h
+++
@@ -1,7 +0,0 @@
-#ifndef AUTOBUILDCOUNT
-#define AUTOBUILDCOUNT
-#define BUILDCOUNT_NUM  15
-#define BUILDCOUNT_STR "15"
-#define GITCTR_NUM      36
-#define GITHASH_STR    "36-gdf10da0-modified"
-#endif
