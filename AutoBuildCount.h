c2f581994032d12d9a2ae053ec92416476532d31
Fish (David B. Trout) <fish@infidels.org>
2016-12-26 14:50:16 -0800

    Initial repository setup

---
+++ AutoBuildCount.h
@@ -0,0 +1,7 @@
+#ifndef AUTOBUILDCOUNT
+#define AUTOBUILDCOUNT
+#define BUILDCOUNT_NUM  15
+#define BUILDCOUNT_STR "15"
+#define GITCTR_NUM      36
+#define GITHASH_STR    "36-gdf10da0-modified"
+#endif
